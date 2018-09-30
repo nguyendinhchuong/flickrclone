5c005f6edaafb5220eaeb3871862fcd05825b00f
Nguyen Dinh Chuong <nguyendinhchuong2607@gmail.com>
2018-10-01 02:01:36 +0700

    image rendered

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
 import Axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroller';
 library.add(faStroopwafel)
 class Header extends Component {
 
@@ -44,19 +47,73 @@ class Header extends Component {
                                 </li>
                             </ul>
                             <form className="form-inline my-2 my-lg-0">
-                                <input className="" type="search" placeholder="Ảnh, mọi người hoặc nhóm" aria-label="Search" />                                
+                                <input className="" type="search" placeholder="Ảnh, mọi người hoặc nhóm" aria-label="Search" />
                             </form>
-                            <ul className="navbar-nav mr-auto">
+                            <ul className="navbar-nav view">
                                 <li className="nav-item">
-                                    <a className="nav-link icon-custom"><FontAwesomeIcon icon="cloud-upload-alt" /></a>
+                                    <a className="nav-link icon-custom"><FontAwesomeIcon icon={faCloudUploadAlt} /></a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link icon-custom"><FontAwesomeIcon icon="bell" /></a>
+                                    <a className="nav-link icon-custom notif"><FontAwesomeIcon icon={faBell} /></a>
                                 </li>
                             </ul>
                         </div>
                     </nav>
                 </div>
+            </div>
+
+        )
+    }
+}
+class Content extends Component {
+    state = {
+        photos: [],
+        isHovering: false
+    }
+    componentDidMount() {
+        Axios.get(`https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=4dd2479d98c250c84c6075528a3292ca&per_page=20&page=1&format=json&nojsoncallback=1&auth_token=72157673983239418-41eb6db34763c6c4&api_sig=15f2914e1620a9d0d4f26cfaa936ffb1`)
+            .then(res => {
+                const photos = res.data.photos.photo;
+                this.setState({ photos });
+            })
+
+    }
+    getOwnerName(ownerid) {
+        Axios.get(`https://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key=4dd2479d98c250c84c6075528a3292ca&user_id=` + ownerid + `&format=json&nojsoncallback=1`)
+            .then(res => {
+                return res.data.person;
+            })
+    }
+    render() {
+        // let photo_list = this.state.photos.map((photo)=>{
+        //     return 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
+        // })
+        return (
+            <div className="container">
+                <InfiniteScroll
+                    pageStart={0}
+                    hasMore={true}
+                    element="div"
+                    initialLoad={true}
+                    loader={<div className="loader" key={0}>Loading ...</div>}
+                    useWindow={false}
+                    loadMore={
+                        this.componentDidMount()
+                    }
+                >
+                    <div className="photo-list-view">
+                        {this.state.photos.map(photo =>
+                            <div className="">
+                                <img src={'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg'} className="img-responsive" alt="" />
+                                <div className="interation-bar">
+                                    <div class="text">
+                                        <a>{photo.title} </a>
+                                        <a>by {this.getOwnerName(photo.owner)}</a>
+                                    </div>
+                                </div>
+                            </div>)}
+                    </div>
+                </InfiniteScroll>
             </div>
 
         )
@@ -70,6 +127,7 @@ class Page extends Component {
         return (
             <div>
                 <Header />
+                <Content />
             </div>
         )
     }
@@ -77,3 +135,4 @@ class Page extends Component {
 }
 
 export default Page;
+//<img src={'https://farm' + {photo.farm} + '.staticflickr.com/' + {photo.server} + '/' + {photo.id} + '_' + {photo.secret} + '.jpg'} className="img-responsive" alt="" />
