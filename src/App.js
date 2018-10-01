c58a18aaf878d1c876d7e6252e5787edf3466c49
chuongnd <nguyendinhchuong2607@gmail.com>
2018-10-01 18:22:54 +0700

    infinite scroll done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons'
 import { faBell } from '@fortawesome/free-solid-svg-icons'
 import Axios from 'axios';
 import InfiniteScroll from 'react-infinite-scroller';
+import Gallery from 'react-grid-gallery';
+import ProTypes from 'prop-types'
 library.add(faStroopwafel)
+
+const api = {
+    baseUrl: 'https://api.flickr.com/services/rest/',
+    api_key: '97625b576d0b7a36de0e967ba2ebb146'
+}
+
 class Header extends Component {
 
     render() {
@@ -65,61 +73,129 @@ class Header extends Component {
         )
     }
 }
-class Content extends Component {
-    state = {
-        photos: [],
-        isHovering: false
-    }
-    componentDidMount() {
-        Axios.get(`https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=4dd2479d98c250c84c6075528a3292ca&per_page=20&page=1&format=json&nojsoncallback=1&auth_token=72157673983239418-41eb6db34763c6c4&api_sig=15f2914e1620a9d0d4f26cfaa936ffb1`)
-            .then(res => {
-                const photos = res.data.photos.photo;
-                this.setState({ photos });
-            })
 
+class Content extends Component {
+    // state = {
+    //     photos: [],
+    //     isHovering: false
+    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            photos: [],
+            hasMoreItems: true,
+            nextHref: null,
+            page_pos: 1
+        }
     }
-    getOwnerName(ownerid) {
-        Axios.get(`https://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key=4dd2479d98c250c84c6075528a3292ca&user_id=` + ownerid + `&format=json&nojsoncallback=1`)
+
+    loadItems(page) {
+        let self = this;
+        let url = api.baseUrl + '?method=flickr.interestingness.getList&api_key=' + api.api_key + '&per_page=20&page=' + self.state.page_pos + '&format=json&nojsoncallback=1';
+        if (this.state.nextHref) {
+            url = this.state.nextHref;
+        }
+        Axios.get(url)
             .then(res => {
-                return res.data.person;
+                if (res) {
+                    const photos_api = res.data.photos.photo;
+                    let photos = self.state.photos;
+                    photos_api.map((image) => {
+                        photos.push(image);
+
+                    })
+                    if (url) {
+                        self.setState({
+                            photos: photos,
+                            nextHref: res.next_href,
+                            page_pos: self.state.page_pos+1
+                        })
+                    } else {
+                        self.setState({
+                            hasMoreItems: false
+                        });
+                    }
+                }
             })
     }
+    // componentDidMount() {
+    //     Axios.get(`https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=4dd2479d98c250c84c6075528a3292ca&per_page=20&page=1&format=json&nojsoncallback=1`)
+    //         .then(res => {
+    //             const photos = res.data.photos.photo;
+    //             this.setState({ photos });
+    //         })
+
+    // }
+    // getOwnerName(ownerid) {
+    //     Axios.get(`https://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key=4dd2479d98c250c84c6075528a3292ca&user_id=` + ownerid + `&format=json&nojsoncallback=1`)
+    //         .then(res => {
+    //             return res.data.person;
+    //         })
+    // }
+
     render() {
         // let photo_list = this.state.photos.map((photo)=>{
         //     return 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
         // })
+        var items = [];
+        this.state.photos.map((photo, i) => {
+            items.push(
+                <div className="track" key={i}>
+                    <a >
+                        <img src={'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg'} />
+                        <p className="title">{photo.owner}</p>
+                    </a>
+                </div>
+            );
+        })
         return (
             <div className="container">
                 <InfiniteScroll
                     pageStart={0}
-                    hasMore={true}
-                    element="div"
+                    hasMore={this.state.hasMoreItems}
                     initialLoad={true}
-                    loader={<div className="loader" key={0}>Loading ...</div>}
-                    useWindow={false}
+                    loader={<div className="lds-ring"><div></div><div></div><div></div><div></div></div>}
                     loadMore={
-                        this.componentDidMount()
+                        this.loadItems.bind(this)
                     }
                 >
-                    <div className="photo-list-view">
+                    {/* <div className="photo-list-view">
                         {this.state.photos.map(photo =>
                             <div className="">
                                 <img src={'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg'} className="img-responsive" alt="" />
-                                <div className="interation-bar">
+                                <div className="interaction-bar">
                                     <div class="text">
                                         <a>{photo.title} </a>
-                                        <a>by {this.getOwnerName(photo.owner)}</a>
+                                        
                                     </div>
                                 </div>
                             </div>)}
+                    </div> */}
+                    <div>
+                        {items}
                     </div>
                 </InfiniteScroll>
             </div>
 
         )
     }
-}
 
+}
+// Content.propTypes = {
+//     images: ProTypes.arrayOf(
+//         ProTypes.shape({
+//             src: PropTypes.string.isRequired,
+//             thumbnail: PropTypes.string.isRequired,
+//             srcset: PropTypes.array,
+//             caption: PropTypes.string,
+//             thumbnailWidth: PropTypes.number.isRequired,
+//             thumbnailHeight: PropTypes.number.isRequired
+//         })
+//     ).isRequired
+// }
+// Content.defaultProps = {
+//     images: shuffleArray()
+// }
 class Page extends Component {
 
 
